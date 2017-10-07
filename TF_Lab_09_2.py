c2ac27d59002c63497e819a04bbcd4615b1c021a
leedongmin <kid33629@gmail.com>
2017-10-07 19:52:41 +0900

    Change code because of tensorboard

--- TF_Lab_09_2.py
+++ TF_Lab_09_2.py
@@ -14,18 +14,18 @@ with tf.name_scope("layer1") as scope1:
     b1 = tf.Variable(tf.random_normal([2]), name='bias1')
     layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 
-    w1_hist = tf.summary.histogram("weight1", W1)
-    b1_hist = tf.summary.histogram("biases1", b1)
-    layer1_hist = tf.summary.histogram("layer1", layer1)
+    # w1_hist = tf.summary.histogram("weight1", W1)
+    # b1_hist = tf.summary.histogram("biases1", b1)
+    # layer1_hist = tf.summary.histogram("layer1", layer1)
 
 with tf.name_scope("layer2") as scope2:
     W2 = tf.Variable(tf.random_normal([2,1]), name='weight2')
     b2 = tf.Variable(tf.random_normal([1]), name='bias2')
     H = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
-    w2_hist = tf.summary.histogram("weight2", W2)
-    b2_hist = tf.summary.histogram("biases2", b2)
-    layer2_hist = tf.summary.histogram("H", H)
+    # w2_hist = tf.summary.histogram("weight2", W2)
+    # b2_hist = tf.summary.histogram("biases2", b2)
+    # layer2_hist = tf.summary.histogram("H", H)
 
 with tf.name_scope("cost") as scope3:
     cost = -tf.reduce_mean(Y*tf.log(H) + (1-Y)*tf.log(1-H))
@@ -36,12 +36,16 @@ with tf.name_scope("train") as scope4:
 
 predicted = tf.cast(H > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
-accuracy_summ = tf.summary.scalar("accuracy", accuracy)
+accuracy_sum = tf.summary.scalar("accuracy", accuracy)
 
 with tf.Session() as sess:
     merged_summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter("/path/to/logs")
+    writer = tf.summary.FileWriter("/logs/xor_logs_r0.1")
     writer.add_graph(sess.graph)
+    # summary마다 업데이트를 계속 하겠다.
+    # 내가 몇번의 한번씩 merge를 하겠다.
+    # 매 회마다 기록을 하겠다.
+    # add_summary가 name_scope내용을 계속 업데이트 하는 것, 적어나가는 것
 
     sess.run(tf.global_variables_initializer())
 
@@ -54,4 +58,3 @@ with tf.Session() as sess:
 
     h, c, a = sess.run([H, predicted, accuracy], feed_dict={X : x_data, Y : y_data})
     print(h, c, a)
-
