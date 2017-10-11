cc5669625fb2a73d329a24ccf275767c9ac0bd8d
leedongmin <kid33629@gmail.com>
2017-10-11 14:41:00 +0900

    Change code because I inserted dropout code

--- TF_Lab_10_1.py
+++ TF_Lab_10_1.py
@@ -5,24 +5,41 @@ import random
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+# parameters
+keep_prob = tf.placeholder(tf.float32)
+# dropout (keep_prob) rate 0.7 on training, but should be 1 for testing
+# 밑에서 0.7로 트레이닝을 시키고, 테스팅할때는 1로 한다.
+training_epochs = 15
+# epoch이라는 것은 전체를 두고 1번 돌리는 값
+# 여기서는 총 15번을 돌린다고 한다.
+# 많을수록 좋다.
+batch_size = 100
+# 데이터양이 크기때문에 한번에 다 학습시키지 않고 나눠서 학습 시킨다.
+# batch_size = 100이 바로 그 몇 개씩 나누는 지 정하는 것
+
+# input place holders
 X = tf.placeholder(tf.float32, shape=[None, 784])
 Y = tf.placeholder(tf.float32, shape=[None, 10])
 
 W1 = tf.get_variable("W1", shape=[784, 512], initializer=tf.contrib.layers.xavier_initializer())
 b1 = tf.Variable(tf.random_normal([512]))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
 W2 = tf.get_variable("W2", shape=[512, 512], initializer=tf.contrib.layers.xavier_initializer())
 b2 = tf.Variable(tf.random_normal([512]))
 L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
 W3 = tf.get_variable("W3", shape=[512, 512], initializer=tf.contrib.layers.xavier_initializer())
 b3 = tf.Variable(tf.random_normal([512]))
 L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 
 W4 = tf.get_variable("W4", shape=[512, 512], initializer=tf.contrib.layers.xavier_initializer())
 b4 = tf.Variable(tf.random_normal([512]))
 L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
 W5 = tf.get_variable("W5", shape=[512, 10], initializer=tf.contrib.layers.xavier_initializer())
 b5 = tf.Variable(tf.random_normal([10]))
@@ -31,18 +48,8 @@ H = tf.matmul(L4, W5) + b5
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=H, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
-keep_prob = tf.placeholder(tf.float32)
-
 is_correct = tf.equal(tf.arg_max(H, 1), tf.arg_max(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-
-training_epochs = 15
-# epoch이라는 것은 전체를 두고 1번 돌리는 값
-# 여기서는 총 15번을 돌린다고 한다.
-# 많을수록 좋다.
-batch_size = 100
-# 데이터양이 크기때문에 한번에 다 학습시키지 않고 나눠서 학습 시킨다.
-# batch_size = 100이 바로 그 몇 개씩 나누는 지 정하는 것
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
